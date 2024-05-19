8f46e347535d9bd529c2e49c4c51e6acc5382b70
RebeccaSchmelzer <beca.dance@gmail.com>
2024-05-18 21:00:36 -0700

    listings view and url

--- listings/urls.py
+++ listings/urls.py
@@ -7,4 +7,5 @@ app_name = 'listings'
 urlpatterns = [
     path('', views.index, name='index'),
     path('greetings', views.greetings, name='greetings'),
+    path('all_listings/', views.all_listings, name='all_listings'),
 ]
