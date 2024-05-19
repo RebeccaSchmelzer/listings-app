a336621cb586d5aea94a9be5a033c275fc183d66
RebeccaSchmelzer <beca.dance@gmail.com>
2024-05-18 22:48:14 -0700

    implement new list form

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Listings
+from .forms import ListingForm
 
 def index(request):
     return render(request, 'listings/index.html')
@@ -13,3 +14,16 @@ def all_listings(request):
         'all_listings': all_listings
     }
     return render(request, 'listings/all_listings.html', context)
+
+
+def new_listing(request):
+    if request.method != 'POST':
+        form = ListingForm()
+    else:
+        form = ListingForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('listings:all_listings')
+      
+    context = {'form': form}
+    return render(request, 'listings/new_listing.html', context)
