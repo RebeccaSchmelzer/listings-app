7e950c8be2a7374951e0afcf54cb4eb0f9b52c28
RebeccaSchmelzer <beca.dance@gmail.com>
2024-05-18 21:30:21 -0700

    created new listing form and url

--- listings/urls.py
+++ listings/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('greetings', views.greetings, name='greetings'),
     path('all_listings/', views.all_listings, name='all_listings'),
+    path('new_listing/', views.new_listing, name='new_listing'),
 ]
